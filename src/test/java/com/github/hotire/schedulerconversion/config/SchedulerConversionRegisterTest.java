d64ff1ddb360af4e9d465b7c3f3e9be3832c504b
hotire <gngh0101@gmail.com>
2019-09-08 23:07:29 +0900

    Add test register in SchedulerConversionRegister

--- src/test/java/com/github/hotire/schedulerconversion/config/SchedulerConversionRegisterTest.java
+++ src/test/java/com/github/hotire/schedulerconversion/config/SchedulerConversionRegisterTest.java
@@ -1,7 +1,51 @@
 package com.github.hotire.schedulerconversion.config;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+import com.github.hotire.schedulerconversion.web.SchedulerConversionWebFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.lang.Nullable;
+
+@RunWith(MockitoJUnitRunner.class)
 public class SchedulerConversionRegisterTest {
 
+  @Mock
+  private Environment environment;
+
+  @Mock
+  private AnnotationMetadata annotationMetadata;
+
+  @Test
+  public void registerBeanDefinitions() {
+    // Given
+    final BeanDefinitionRegistry beanDefinitionRegistry = new GenericApplicationContext();
+    final GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+    final SchedulerConversionRegister register = new SchedulerConversionRegister() {
+      @Override
+      protected GenericBeanDefinition createBean(@Nullable Class<?> beanClass) {
+        return beanDefinition;
+      }
+    };
+    register.setEnvironment(environment);
+
+    // When
+    when(environment.getProperty("scheduler.conversion.mode")).thenReturn("include");
+    when(environment.getProperty("scheduler.conversion.patterns")).thenReturn("");
+
+    register.registerBeanDefinitions(annotationMetadata, beanDefinitionRegistry);
+    final BeanDefinition result = beanDefinitionRegistry.getBeanDefinition(SchedulerConversionWebFilter.class.getSimpleName());
+
+    // Then
+    assertThat(result).isEqualTo(beanDefinition);
+  }
 }
